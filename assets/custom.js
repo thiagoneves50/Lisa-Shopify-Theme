27bf0e96ee4b6598d5c5333aa08284f9dfa89199
Thiago Neves <thiagoneves50@gmail.com>
2022-05-31 15:20:28 -0300

    Add slider section

--- assets/custom.js
+++ assets/custom.js
@@ -1,3 +1,4 @@
+// START - Tabs Section
 console.log("hello from tabs");
 const titleBtns = document.querySelectorAll('.title-btn');
 console.log('titleBtns:', titleBtns)
@@ -24,3 +25,33 @@ titleBtns.forEach((titleBtn, index) => {
     changeTab(index);
   })
 })
+
+// END - Tabs Section
+
+// START - Slick Slider Section
+$(document).ready(function () {
+	$(".testimonial-slider").slick({
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 1,
+		dots: true,
+		arrows: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1,
+				},
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	});
+});
+// END - Slick Slider Section
