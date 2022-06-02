2d3a089e84a5241c9c794b01a6df6c3f5a5ac19d
Thiago Neves <thiagoneves50@gmail.com>
2022-06-02 14:42:17 -0300

    fetch post request working

--- assets/custom.js
+++ assets/custom.js
@@ -55,3 +55,106 @@ $(document).ready(function () {
 	});
 });
 // END - Slick Slider Section
+
+// START - Ajax Cart
+console.log('this is ajax cart');
+
+const ajaxCartContainer = document.querySelector('#ajax-cart-template');
+const getItems = document.querySelector('.cart-button');
+const addItem = document.querySelectorAll('.cart-add-button');
+const cartItems = document.querySelectorAll('input[data-line-item]')
+const lineNumbers = [];
+cartItems.forEach((element, index) => {	
+	let lineStr = element.dataset.lineItem
+	let line = parseInt(lineStr)
+	lineNumbers[index] = line
+	console.log(line);
+	console.log(lineNumbers);
+});
+
+getItems.addEventListener('click', function(){
+	console.log('getting cart items');
+	fetch('/?view=cart-drawer')
+		.then(response => { 
+			return response.text() 
+		})
+		.then(data => {
+			console.log(data);
+			ajaxCartContainer.innerHTML = data
+			
+		})
+})
+// addItem.addEventListener('click', function(){
+// 	console.log('adding 1 item');
+
+// 	let formData = {
+// 		'updates': [{
+// 		 'id': 36110175633573,
+// 		 'quantity': 2
+// 		 }]
+// 	 };	
+
+
+// 	fetch('/?view=cart-drawer')
+// 		.then(response => { 
+// 			return response.text() 
+// 		})
+// 		.then(data => {
+// 			console.log(data);
+// 			ajaxCartContainer.innerHTML = data
+			
+// 		})
+// })
+addItem.forEach(item => {
+	item.addEventListener('click', function(event){
+		console.log(getLineCartItem(event))
+		console.log(getLineCartItemQty(event))
+		let cartLineItem = getLineCartItem(event)
+		let cartLineItemQty = getLineCartItemQty(event)
+
+		updateQuantity(cartLineItem, ++cartLineItemQty)
+		
+		
+	})	
+});
+
+function getLineCartItem(event) {
+	return parseInt(event.target.previousElementSibling.dataset.lineItem);
+}
+function getLineCartItemQty(event) {
+	return parseInt(event.target.previousElementSibling.value);
+}
+
+function updateQuantity(line, qty) {
+	fetch('/cart/change.js', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({
+			line: line,
+			quantity: qty
+		})
+	}).then(function (response) {
+		console.log('iniciando');
+		return response.json();
+	}).then(function (data) {
+		// fire javascript event on window
+		setTimeout(() => {
+			console.log(data);
+			fetch('/?view=cart-drawer')
+		.then(response => { 
+			return response.text() 
+		})
+		.then(data => {
+			console.log(data);
+			ajaxCartContainer.innerHTML = data
+			
+		})
+
+		}, 500)
+	})["catch"](function (error) {
+		console.error('Error:', error);
+	});
+}
+
